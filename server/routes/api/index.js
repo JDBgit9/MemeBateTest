42bf5b3fffd64051deda5542c8f57a0a79d1eec1
JDBgit9 <info@jamesdbeeks.com>
2020-12-17 18:45:17 -0500

    heroku

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,18 +1,18 @@
-const PORT = process.env.PORT || 3000;
-app.listen(port, () => console.log('listening on port 5000'));
-const router = require("express").Router();
-const mediaRoutes = require("./media");
-const userRoutes = require("./user");
-const memebaterRoutes = require("./memebater");
+// const PORT = process.env.PORT || 3000;
+// app.listen(port, () => console.log('listening on port 5000'));
+// const router = require("express").Router();
+// const mediaRoutes = require("./media");
+// const userRoutes = require("./user");
+// const memebaterRoutes = require("./memebater");
 
-// Book routes
-router.use("/media", mediaRoutes);
-router.use("/user", userRoutes);
-router.use("/memebater", userRoutes);
+// // Book routes
+// router.use("/media", mediaRoutes);
+// router.use("/user", userRoutes);
+// router.use("/memebater", userRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+// // If no API routes are hit, send the React app
+// router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//   });
 
-module.exports = router;
+// module.exports = router;
