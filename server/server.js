3881dcf877cc97eaf6e3da7050f417731e4bb9bf
Jason Rice <jrice@entrata.com>
2020-12-22 09:16:14 -0700

    Resolved conflicting ports and built a script to concurrently run the server and client in either dev or prod (Heroku) mode.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 require("dotenv/config");
-const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
-// const PORT = 4041;
+const PORT = 4041;
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const Media = require("./models/media");
 const Memebater = require("./models/memebater");
 const mongoose = require("mongoose");
 const distDir = __dirname + "/dist/";
-
 
 const user = new MongoClient(
   "mongodb+srv://110120MongoDBatlas_:110120MongoDBatlas_@workout-tracker.bkgq3.mongodb.net/?retryWrites=true&w=majority"
@@ -25,7 +23,6 @@ mongoose.connection.once("open", () => {
 });
 
 app.use(cors({ origin: true }));
-app.listen(process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.static(distDir));
 
@@ -41,6 +38,7 @@ app.get("/media", async (request, response) => {
     response.status(500).send(error);
   }
 });
+
 app.post("/media", async (request, response) => {
   const media = new Media(request.body);
 
@@ -69,6 +67,7 @@ async function listDatabases(client) {
   console.log("Databases:");
   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
 }
+
 app.get("/memebater", async (request, response) => {
   console.log("MEMEBATER IS BEING REQUESTED");
   try {
@@ -81,6 +80,7 @@ app.get("/memebater", async (request, response) => {
     response.status(500).send(error);
   }
 });
+
 app.post("/memebater", async (request, response) => {
   const memebater = new Memebater(request.body);
 
